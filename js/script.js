a36a44f15f85d90e6c8d26f7513975df3b4c8e90
min6683 <dev.sm6683@gmail.com>
2022-05-15 01:45:16 +0900

    기본틀 완성

---
+++ js/script.js
@@ -0,0 +1,56 @@
+window.onload = function () {
+    let sw_cc = new Swiper('.sw-cc', {
+        slidesPerView: 3,
+        loop: false,
+        pagination: {
+            el: '.sw-cc-pg',
+            clickable: true,
+        },
+        spaceBetween: 30,
+    });
+    $(".hover").mouseleave(
+        function () {
+            $(this).removeClass("hover");
+        }
+    );
+
+    // 위로 가기
+    let gotop = $('.gotop');
+    gotop.click(function (event) {
+        event.preventDefault();
+        $('html').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    let gnb_a = $('.gnb a');
+    let main_btn = $('.main-btn');
+    $(document).ready(function ($) {
+
+        $(gnb_a).click(function (event) {
+            event.preventDefault();
+            console.log(gnb_a);
+            $('html,body').animate({
+                scrollTop: $(this.hash).stop().offset().top - 100
+            }, 500);
+
+        });
+
+    });
+    $(document).ready(function ($) {
+
+        $(main_btn).click(function (event) {
+            event.preventDefault();
+            console.log(main_btn);
+            $('html,body').animate({
+                scrollTop: $(this.hash).stop().offset().top - 100
+            }, 500);
+
+        });
+
+    });
+};
+
+$(document).ready(function () {
+
+});
