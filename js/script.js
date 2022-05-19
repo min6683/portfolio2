ec9c26ed51336f907c9c34317f203d4536a7c500
min6683 <dev.sm6683@gmail.com>
2022-05-19 17:48:30 +0900

    반응형 메뉴 구현

--- js/script.js
+++ js/script.js
@@ -25,6 +25,7 @@ window.onload = function () {
 
     let gnb_a = $('.gnb a');
     let main_btn = $('.main-btn');
+    let submenu_a = $('.submenu a');
     $(document).ready(function ($) {
 
         $(gnb_a).click(function (event) {
@@ -34,23 +35,8 @@ window.onload = function () {
             }, 500);
 
         });
-
-    });
-    let menu_ham = $('.icon-menu');
-    let submenu = $('.submenu');
-    $(document).ready(function(){
-        $(menu_ham).click(function(event){
-            event.preventDefault()
-            $('menu_ham').scrollTop('submenu');
-        })
-    })
-
-
-    $(document).ready(function ($) {
-
-        $(main_btn).click(function (event) {
+        $(submenu_a).click(function (event) {
             event.preventDefault();
-            console.log(main_btn);
             $('html,body').animate({
                 scrollTop: $(this.hash).stop().offset().top - 100
             }, 500);
@@ -58,6 +44,38 @@ window.onload = function () {
         });
 
     });
+    // let menu_ham = $('.icon-menu');
+    let submenu = $('.submenu');
+    // let submenu_i = $('.submenu > i');
+    let navbar_toggler = $('.navbar-toggler');
+    let navbar_collapse = $('.navbar-collapse');
+
+    $(document).ready(function () {
+        // $(".navbar-toggler").on('click', function () {
+        //     $(this).toggleClass('active');
+        // });
+        // $(".navbar-toggler").on('click', function () {
+        //     submenu.css('display', 'block');
+        // });
+        $('#btn').click(function(){
+            $('.m-menu').toggleClass('m-menu-active');
+        })
+        
+    });
+
+
+
+
+
+    $(main_btn).click(function (event) {
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop: $(this.hash).stop().offset().top - 100
+        }, 500);
+
+    });
+
+
 };
 
 $(document).ready(function () {
